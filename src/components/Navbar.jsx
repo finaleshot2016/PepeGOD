1583caa1dab99bd1895a428cbe52ca9689fe10e6
The Specialist_JS <103410982+finaleshot2016@users.noreply.github.com>
2022-10-12 04:18:37 +0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,10 +17,10 @@ const Navbar = () => {
         </div>
         <div className='hidden md:flex'>
           <ul className='flex text-[#276400] items-center'>
-            <li><a href="/" target="_blank" rel="noopener noreferrer">TELEGRAM</a></li>
+            <li><a href="https://t.me/GodOfMemesERC" target="_blank" rel="noopener noreferrer">TELEGRAM</a></li>
             <li><a href="https://twitter.com/GodofMemesERC20" target="_blank" rel="noopener noreferrer">TWITTER</a></li>
             <li><a href="https://medium.com/@PepeFrog/god-of-memes-24c05f8b07ec" target="_blank" rel="noopener noreferrer">MEDIUM</a></li>
-            <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2 mt-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"><a href="/" target="_blank" rel="noopener noreferrer">BUY NOW!</a></button>
+            <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2 mt-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"><a href="https://app.uniswap.org/#/swap?outputCurrency=0x746007261627fce35137918ed4cb8331a8f924a1" target="_blank" rel="noopener noreferrer">BUY NOW!</a></button>
           </ul>
         </div>
 
