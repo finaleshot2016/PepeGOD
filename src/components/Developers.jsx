96e5f14d99dffa41fa4277169c6967a34a97019e
The Specialist_JS <103410982+finaleshot2016@users.noreply.github.com>
2022-10-16 07:21:41 +0800

    Update Developers.jsx

--- src/components/Developers.jsx
+++ src/components/Developers.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import './dev.css';
-import terminal from '../assets/roadmap.png'
-import terminal4 from '../assets/pepegod3.png'
+import terminal from '../assets/pepeblessings.png'
+
 
 const Developers = () => {
   return (
-    <div id="tokenomics" className='hero-image'>
- <img className='justify-center' src={terminal} alt="/" />
+    <div id="tokenomics" className='text-center bg-lime-100'>
+ <img className='justify-center' src={terminal} alt="/" /> 
+ <p className='text-black text-lg'>
+Pepe God's goal is to help the poor chads in this world of crypto. By buybacking 2ETH and airdroping tokens.
+<br /><br />
+<b>So how can He help?</b><br />
+By randomly Airdroping 3k $PEPE to the holders. (Worth 3ETH in 1M MC)<br /><br />
 
- <div> <img className='justify-center' src={terminal4} alt="/" />
-<iframe className='justify-center py-4 w-full aspect-video' src="https://dexscreener.com/ethereum/0xe0f908154395f0ba4a80d85f0c41fb6909e4ec8f" title="dexchart"></iframe>
-          </div>
+<b>How can we get airdrop?</b><br />
+You are already eligible by holding 500-1k $PEPE. (1K above and 500 below is not eligible)<br /><br />
+
+<b>What is the goal of this?</b><br />
+It will pump the $PEPE holders, Whales will distribute their tokens to different wallets,<br /> if they want to win, therefore they will not dump their whole bag and destroy the chart.
+<br /><br /></p>  
 
     </div>
   );
