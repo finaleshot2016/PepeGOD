0d099eb384f8ac0aa8a488465739a7ead8c8ad2e
The Specialist_JS <103410982+finaleshot2016@users.noreply.github.com>
2022-10-16 07:29:05 +0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,6 +20,7 @@ const Navbar = () => {
             <li><a href="/" target="_blank" rel="noopener noreferrer">TELEGRAM</a></li>
             <li><a href="https://twitter.com/GodofMemesERC20" target="_blank" rel="noopener noreferrer">TWITTER</a></li>
             <li><a href="https://medium.com/@PepeFrog/god-of-memes-24c05f8b07ec" target="_blank" rel="noopener noreferrer">MEDIUM</a></li>
+           <li><a href="mailto:godofmemespepe2022@gmail.com" target="_blank" rel="noopener noreferrer">Contact Us</a></li>
             <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2 mt-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"><a href="https://pepegodapp.com/" target="_blank" rel="noopener noreferrer">DASHBOARD</a></button>
           </ul>
         </div>
